69b893d6db4534e6d22d6335db3b1e24e6c08794
Lucas Souza <lucas.souza@primedb.com.br>
2024-05-13 20:14:16 +0000

    testing new output

--- func.py
+++ func.py
@@ -3,7 +3,7 @@ import json
 from fdk import response
 
 def handler(ctx, data: io.BytesIO=None):
-    name = "World"
+    name = "Miami :D"
     return response.Response(
         ctx, response_data=json.dumps(
             {"message": "Hello {0}".format(name)}),
