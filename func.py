1ad8a2f6d96f722c1a208cb3a731ac01d2668523
Lucas Souza <lucasouza191141@gmail.com>
2024-06-05 09:53:06 -0300

    Iniciado desenvolvimento da automacao Topdesk

--- func.py
+++ func.py
@@ -1,15 +1,18 @@
 import io
 import json
 from fdk import response
-from helpers import initialize_process, load_sheets, first_transformation, second_transformation, third_transformation, fourth_transformation, fifth_transformation, sixth_transformation, extra_transformation, seventh_transformation, eight_transformation
+from helpers import set_initial_config, load_topdesk_data, load_tables, first_transformation, second_transformation, third_transformation, save_to_bucket
 
 def handler(ctx, data: io.BytesIO=None):
     try:
         print("> Iniciando processo...")
-        obj_client, namespace, bucket, main_file = initialize_process()
+        url, topdesk_auth, headers, obj_client  = set_initial_config()
 
-        print("> Lendo planilhas")
-        x = load_sheets(namespace, bucket, main_file)
+        print("> Lendo dados do Topdesk...")
+        dados = load_topdesk_data(url, topdesk_auth, headers)
+
+        print("> Lendo tabelas do Topdesk")
+        x = load_tables(topdesk_auth, url, headers, dados)
 
         print("> 1a Transformação")
         x = first_transformation(x)
@@ -20,26 +23,8 @@ def handler(ctx, data: io.BytesIO=None):
         print("> 3a Transformação")
         x = third_transformation(x)
 
-        print("> Extra data formation")
-        x = extra_date_transformation(x)
-
-        print("> 4a Transformação")
-        x = fourth_transformation(x)
-
-        print("> 5a Transformação")
-        x = fifth_transformation(x)
-
-        print("> 6a Transformação")
-        x = sixth_transformation(x)
-
-        print("> Transformação Extra (problema com timestamp Spark e Pandas)")
-        x = extra_transformation(x)
-
-        print("> 7a Transformação")
-        x = seventh_transformation(x)
-
         print("> Ùltima Transformação")
-        eight_transformation(x, obj_client)
+        save_to_bucket(obj_client, x)
 
         print("# Processo finalizado com sucesso :D")
         return response.Response(
@@ -54,3 +39,42 @@ def handler(ctx, data: io.BytesIO=None):
                 {"message": "{0}".format(str(ex))}),
             headers={"Content-Type": "application/json"}
     )
+
+"""
+def handler(ctx, data: io.BytesIO=None):
+    try:
+        print("> Iniciando processo...")
+        url, topdesk_auth, headers, obj_client  = set_initial_config()
+
+        print("> Lendo dados do Topdesk...")
+        dados = load_topdesk_data(url, topdesk_auth, headers)
+
+        print("> Lendo tabelas do Topdesk")
+        x = load_tables(topdesk_auth, url, headers, dados)
+
+        print("> 1a Transformação")
+        x = first_transformation(x)
+
+        print("> 2a Transformação")
+        x = second_transformation(x)
+
+        print("> 3a Transformação")
+        x = third_transformation(x)
+
+        print("> Ùltima Transformação")
+        save_to_bucket(obj_client, x)
+
+        print("# Processo finalizado com sucesso :D")
+        return response.Response(
+            ctx, response_data=json.dumps(
+                {"message": "Arquivos processados de parque atualizados no bucket TRUSTED com sucesso!"}),
+            headers={"Content-Type": "application/json"}
+        )
+    except Exception as ex:
+        print("> Deu ruim :(, ", str(ex))
+        return response.Response(
+            ctx, response_data=json.dumps(
+                {"message": "{0}".format(str(ex))}),
+            headers={"Content-Type": "application/json"}
+    )
+"""
